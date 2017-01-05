a07b988bd12ff7b983e32f1f8e5fd001520ae022
Molly Arhammar <mollyarhammar@gmail.com>
2017-01-05 21:10:30 +0100

    update service worker

--- desktop/source/serviceWorker.js
+++ desktop/source/serviceWorker.js
@@ -1,23 +1,16 @@
-let config = {
-    staticCacheItems: [
-        '/desktop/source/draggable-window.html',
-        '/desktop/source/expandable-menu-item.html',
-        '/desktop/source/image/chat-icon.png',
-        '/desktop/source/image/memory-icon.png',
-        '/desktop/source/image/gallery-icon.png',
-        '/desktop/source/image/desktop-background.jpg',
-        '/desktop/source/image/draggable-window-border.png',
-        '/desktop/source/js/desktop.js',
-        '/desktop/source/stylesheet/style.css',
-        '/desktop/source/index.html'
-    ]
-};
+/**
+ * A very simple ServiceWorker that caches the
+ * site for offline use.
+ */
 
+
+//keep track of the cache
 let CACHE_VERSION = 'v1';
 
 
 self.addEventListener('install', event => {
 
+    //store some files on first load
     function onInstall () {
         return caches.open(CACHE_VERSION)
             .then(cache => cache.addAll([
@@ -39,6 +32,7 @@ self.addEventListener('install', event => {
 
 });
 
+//clear out the cache if service worker is updated
 self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then(function (cacheNames) {
@@ -53,21 +47,23 @@ self.addEventListener('activate', (event) => {
     );
 });
 
+//handle fetch events by sending back the cached resource if avaliable
 self.addEventListener('fetch', (event) => {
-    function onFetch (event, opts) {
+    function onFetch (event) {
         let request = event.request;
         let acceptHeader = request.headers.get('Accept');
         let resourceType = 'static';
         let cacheKey;
 
-        if (acceptHeader.indexOf('text/html') !== -1) {
+        if (acceptHeader.indexOf('text/html') !== -1) { //make different cashes for different content to retrieve later
             resourceType = 'content';
         } else if (acceptHeader.indexOf('image') !== -1) {
             resourceType = 'image';
         }
 
         cacheKey = resourceType;
-        // Use a cache-first strategy.
+
+        // Use cache first.
         event.respondWith(
             fetchFromCache(event)
                 .catch(() => fetch(request))
@@ -75,9 +71,10 @@ self.addEventListener('fetch', (event) => {
         );
     }
 
+    //add responses to cache to fetch later
     function addToCache (cacheKey, request, response) {
         if (response.ok) {
-            let copy = response.clone();
+            let copy = response.clone(); //copy the response to not use it up
             caches.open(cacheKey).then( cache => {
                 cache.put(request, copy);
             });
@@ -85,15 +82,15 @@ self.addEventListener('fetch', (event) => {
         }
     }
 
+    //get responses from cache
     function fetchFromCache (event) {
         return caches.match(event.request).then(response => {
             if (!response) {
-                // A synchronous error that will kick off the catch handler
                 throw Error('${event.request.url} not found in cache');
             }
             return response;
         });
     }
 
-    onFetch(event, config);
+    onFetch(event);
 });
